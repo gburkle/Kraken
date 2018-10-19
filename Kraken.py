5e703b0ac14a8b38d79644bfdeb4eb32f2db5fc2
BlackConverseHacker <gburkle@users.noreply.github.com>
2018-10-19 14:51:49 -0500

    Update Kraken.
    Fix bugs.
    Added option to force delete and ask for no confirmation. (Careful!)

--- Kraken.py
+++ Kraken.py
@@ -3,7 +3,7 @@
 """
 RELASE THE KRAKEN!!!
 If you release the kraken, it will find and destroy any folder or files you want to eliminate. 
-10/16/2018
+Gerardo - 10/16/2018
 """
 import sys
 import argparse
@@ -21,36 +21,47 @@ except:
 def main():
 	"Release the KRAKEN"
 	parser=argparse.ArgumentParser(description='Remove Files or Folders. (e.g. kraken -path "C:\Program Files" -F "some folder" OR kraken -path "C:\Program Files" -LF "folders.txt")')
-	parser.add_argument('-path','--path',required=True,help='Directory path where to start looking. Use quotes (e.g. "C:\Program Files\folder")',dest='path')
-	parser.add_argument('-F','--folder',required=False,help='Folder name to search and destroy',dest='folder')
-	parser.add_argument('-f','--file',required=False,help='File name to search and destroy',dest='file')
-	parser.add_argument('-LF','--listfolders',required=False,help='Text file with a list of folders to search and destroy',dest='list_of_folders')
-	parser.add_argument('-lf','--listfiles',required=False,help='Text file with a list of files to search and destriy',dest='list_of_files')
+	parser.add_argument('--path',required=True,help='Directory path where to start looking. Use quotes (e.g. "C:\Program Files\folder")',dest='path')
+	parser.add_argument('--folder',required=False,help='Folder name to search and destroy',dest='folder')
+	parser.add_argument('--file',required=False,help='File name to search and destroy',dest='file')
+	parser.add_argument('--listfolders',required=False,help='Text file with a list of folders to search and destroy',dest='list_of_folders')
+	parser.add_argument('--listfiles',required=False,help='Text file with a list of files to search and destriy',dest='list_of_files')
+	parser.add_argument('--force',required=False,help='Don\'t ask for confirmation. Just search and destroy',dest='force',default=False, action='store_true')
 	args=parser.parse_args()
 	
-	#print(args) 
+	print(args) 
 	print("\n				-------  THE KRAKEN   --------- \n")
 
 	if not os.path.isdir(args.path):
 		print("Folder location (" + args.path + ") does not exist. ")
 		print("Use Kraken --help for more information on how to use this program")
 		quit()
+		
 	elif args.folder != None:
 		#print("looking for a folder")
-		find_folder(args.path, args.folder)
+		
+		if args.force != False:
+			find_folder_force(args.path, args.folder)
+		else:
+			find_folder(args.path, args.folder)
+		
 		quit()
+		
 	elif args.file != None:
 		#print("looking for a file")
 		find_file(args.path, args.file)
 		quit()
+		
 	elif args.list_of_folders != None:
 		#print("looking for a list of folders")
 		find_folder_list(args.path, args.list_of_folders)
 		quit()
+		
 	elif args.list_of_files != None:
 		#print("looking for a list of files")
 		find_file_list(args.path, args.list_of_files)
 		quit()
+		
 	else:
 		print("You need to tell the kraken what to search and destroy!!")
 	
@@ -112,6 +123,37 @@ def find_folder(path, name):
 	
 	return
 
+def find_folder_force(path, name):
+	"This will search for the name of a folder, then delete all. No questions asked"
+	print("[+] Kraken will look for all instances of (" + name + ") inside the folder (" + path + ") and delete all findings. (NO QUESTIONS ASKED!!)\n")
+	prey_found = []
+	
+	print("[*] The Kraken is looking for prey.\n")
+	for path, listofdirs, listoffiles in os.walk(path):
+		#print("[*] Current Directory is :",path)
+		#print("			has directories:",listofdirs)
+		#print("			has files:",listoffiles)
+		prey=name.lower()
+		location=path.lower()
+			
+		find = re.compile('\\\(%s)$' % prey)
+		found = find.findall(location)
+			
+		if found:
+			prey_found.append(path)
+		else:
+			continue
+		
+	if prey_found:
+		print("[*] The Kraken found prey!!")
+		print("\n!!! RLEASE THE KRAKEN !!!\n")
+		for folder in prey_found:
+			print("[*] Deleting ("+ folder +") .....", end="", flush=True)
+			result = delete_folder(folder)
+			print("["+result+"]")
+	else:
+		print("[*] The Kraken didn't find any folders with that name")		
+	
 def delete_folder(path):
 	"DELETE FOUND FOLDERS"
 		
