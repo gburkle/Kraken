aa3dc8c6e2b083ae194798518ac3151b5dc1748a
BlackConverseHacker <gburkle@users.noreply.github.com>
2018-10-22 17:48:31 -0500

    Removal of streamer AutoIt Malware.

--- streamer_removal.py
+++ streamer_removal.py
@@ -6,17 +6,37 @@ Remove all instances (Folders and links) of streamerdata malware.
 import re
 import os
 import shutil
+import datetime
+
+
+## GET DATE ANT TIME FOR LOG FILE IN CASE OF ERRORS
+dateandtime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+log_file = "streamer_removal_"+dateandtime+".log"
 
 def main():
-	print("[*] REMOVING ALL INSTANCES OF STREAMER.") 
+	print("[*] REMOVING ALL INSTANCES OF STREAMER.")
+	global log_file
+	
+	## GET PRESENT WORKING DIRECTORY. SCAN WILL START FROM HERE. 
 	dir_path = os.path.dirname(os.path.realpath(__file__))
-	#print(dir_path)
-	devour_streamer(dir_path)
+		
+	## START THE SCAN
+	results = devour_streamer(dir_path)
+	
+	if results == "CLEAN":
+		print("[*] SCAN COMPLETED WITH NO ERRORS")
+	elif results == "ERRORS":
+		print("[*] SCAN COMPLETED WITH ERRORS!!. Check "+log_file+" file for more details.")
+	else:
+		print("\nSomething weird happened.\n")
+	
 
 
 def devour_streamer(path):
-	"Find and destroy streamer malware"
+	"Find and destroy streamer Malware"
 	
+	global log_file
+
 	evil = b'\x73\x00\x74\x00\x61\x00\x72\x00\x74\x00\x20\x00\x73\x00\x74\x00\x72\x00\x65\x00\x61\x00\x6D\x00\x65\x00\x72\x00\x64' \
 		+ b'\x00\x61\x00\x74\x00\x61\x00\x5C\x00\x73\x00\x74\x00\x72\x00\x65\x00\x61\x00\x6D\x00\x65\x00\x72\x00\x2E\x00\x65\x00' \
 		 + b'\x78\x00\x65\x00\x20\x00\x2F\x00\x41\x00\x75\x00\x74\x00\x6F\x00\x49\x00\x74\x00\x33\x00\x45\x00\x78\x00\x65\x00\x63' \
@@ -26,11 +46,7 @@ def devour_streamer(path):
 	
 	#print(evil.decode('utf-8'))
 
-	for path, listofdirs, listoffiles in os.walk(path):
-		#print("[*] Current Directory is :",path)
-		#print("			has directories:",listofdirs)
-		#print("			has files:",listoffiles)
-				
+	for path, listofdirs, listoffiles in os.walk(path):				
 		### EAT THE LINKS 
 		links = []
 		
@@ -59,9 +75,7 @@ def devour_streamer(path):
 					else:
 						continue				
 				except Exception as e:
-					print("[ERROR]")
-					print(e)
-		
+					log_errors(full_link, e)		
 
 		### EAT THE FOLDERS
 		location=path.lower()	
@@ -75,14 +89,15 @@ def devour_streamer(path):
 		else:
 			continue
 	
-	return
-	
+
+	if not os.path.isdir(log_file):
+		return('CLEAN')
+	else:
+		return('ERRORS')
+
 	
 def devour_prey(path, file=False):
 	"DELETE FOUND FOLDERS OR FILES"
-		
-	filehandle = open('streamer_removal.log','a')
-	
 
 	if file == False:
 		try:
@@ -90,17 +105,25 @@ def devour_prey(path, file=False):
 			result = "SUCCESS"
 		except Exception as e:
 			result = "ERROR"
-			filehandle.write("Folder: "+ path +" | Error: "+ str(e))
+			log_errors(path, e)
 	else:
 		try:
 			os.remove(path)
 			result = "SUCCESS"
 		except Exception as e:
 			result = "ERROR"
-			filehandle.write("File: "+ path +" | Error: "+ str(e))
+			log_errors(path, e)
 	
-	filehandle.close()
 	return result
+	
+def log_errors(path, log):
+	"Log errors to streamer_removal.log"
+	
+	global log_file
+	
+	filehandle = open(log_file,'a')
+	filehandle.write("PATH: "+ path +" | ERROR: "+ str(log) +"\n")
+	filehandle.close()
 	
 if __name__=="__main__":
 
